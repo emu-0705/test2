6baa8cdd78008264bb63dcf68f6011eb4109d3f9
emu-0705 <231042@std.hi-joho.ac.jp>
2026-04-28 10:26:20 +0900

    Fix syntax error in Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Hello GitHub! from Eclipse");
         System.out.println("プッシュ成功したら嬉しいな！");
+
+        System.out.println（"編集成功してるかな~~");
 }
 }
